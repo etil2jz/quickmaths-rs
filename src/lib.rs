fc36d1617c739e9bfd3482bcb54d218435790a1e
Etil <81570777+etil2jz@users.noreply.github.com>
2023-11-12 15:49:45 +0100

    change: rename lib because it's funnier

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@
 
 /// Approximation of the sine of an angle using Taylor sequences.
 /// For applications that do not require absolute precision.
-pub fn fm_sin(angle: f64) -> f64 {
+pub fn qm_sin(angle: f64) -> f64 {
     return angle - angle.powi(3) / 6.0 + angle.powi(5) / 120.0 - angle.powi(7) / 5040.0;
 }
 
 /// Approximation of the cosine of an angle using Taylor sequences.
 /// For applications that do not require absolute precision.
-pub fn fm_cos(angle: f64) -> f64 {
+pub fn qm_cos(angle: f64) -> f64 {
     return 1.0 - angle.powi(2) / 2.0 + angle.powi(4) / 24.0 - angle.powi(6) / 720.0;
 }
